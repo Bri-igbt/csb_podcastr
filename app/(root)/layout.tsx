8709a532833f54f6161a08c150e112280382791a
ChimaBright <blakechima@gmail.com>
2025-08-16 19:07:33 +0100

    initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,39 @@
+import LeftSideBar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="relative flex flex-col">
+      <main className="bg-black-3 flex relative">
+        <LeftSideBar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image 
+                src='/icons/logo.svg'
+                alt="logo"
+                width={30}
+                height={30}
+              />
+              <MobileNav />
+            </div>
+
+            <div className="flex flex-col md:pb-14">
+              Toaster (notification popups)
+              {children}
+            </div>
+          </div>
+        </section>
+
+        <RightSidebar />
+      </main>
+    </div>
+  );
+}
